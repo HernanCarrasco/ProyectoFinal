c07cef5f8149e76a7b05a6b66769f3217af10d39
Hernan Carrasco <hernanhcarrasco@gmail.com>
2022-10-08 16:18:54 +0200

    Act2. Corregidos templates y act botones, se intentó la busqueda

--- App/urls.py
+++ App/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
         path("ver_post/", ver_post, name="ver_post"),
         path("crear_blog/", crear_blog, name="crear_blog"),
         path("pages/", pages, name="pages"),
-        path("principal", publicacion, name="principal"),
-        path("busqueda_tema", busqueda , name="busqueda_tema"),
-        path("crear_usuario", usuario , name="crear_usuario"),
+        path("principal/", publicacion, name="principal"),
+        path("busqueda_tema/", busqueda , name="busqueda_tema"),
+        path("crear_usuario/", usuario , name="crear_usuario"),
 ]
